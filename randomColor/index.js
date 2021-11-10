c81aaba6809761882158b4d0a436fc4ba3d6179b
Gökmen Nişancı <nisancigokmen@gmail.com>
2021-11-10 23:33:02 +0300

    Update index.js

--- randomColor/index.js
+++ randomColor/index.js
@@ -8,7 +8,7 @@ randomColorBtn.addEventListener("click", function() {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    const newColor = `rgb(${r},${b},${g})`;
+    const newColor = `rgb(${r},${g},${b})`;
 
     document.body.style.backgroundColor = newColor;
     titleTag.innerText = newColor;
